2aa820dc860b88a7f161118b06d7934f01600fdf
omidparishani <omidparishani@yahoo.com>
2023-09-26 18:03:10 +0330

    blue

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class ClassDirective {
 
   constructor(private elementRef: ElementRef) {
-    this.elementRef.nativeElement.style.backgroundColor = 'red';
+    this.elementRef.nativeElement.style.backgroundColor = 'blue';
   }
 
 }
